a74945367b6e72cd0e9c3574c993f7ea6001503f
Hyun Kyu Seo <hyun@kayhan.space>
2022-06-01 23:32:13 -0400

    Added Webpack + JS dist version

--- src/GameInteraction/AsteroidsKeyBindings.ts
+++ src/GameInteraction/AsteroidsKeyBindings.ts
@@ -1,20 +1,38 @@
 import { mainLoop } from "../GameApp/AsteroidsApp";
 import { Game } from "../GameLogic/AsteroidsGameLogic";
 
+export const gameGoForward = function(bool: boolean){
+    Game.state.KEY_STATUS['arrowup'] = bool;
+}
+
+export const gameTurnLeft = function(bool: boolean){
+    Game.state.KEY_STATUS['arrowleft'] = bool;
+}
+
+export const gameTurnRight = function(bool: boolean){
+    Game.state.KEY_STATUS['arrowright'] = bool;
+}
+
+export const gameFire = function(bool: boolean){
+    Game.state.KEY_STATUS['space'] = bool;
+}
+
+export const handleGamePause = function(){
+    if (Game.state.isPaused) {
+        Game.state.isPaused = false;
+        mainLoop();
+    } else {
+        if(Game.innerState !== ("waiting" || "boot" || "reset")) Game.state.isPaused = true;
+    }
+}
+
 export const gameKeyDown = (e: KeyboardEvent): void => {
     const eKeyVal = e.key.toLowerCase();
     if (Game.state.KEY_STATUS[eKeyVal] !== undefined) {
         const toggleVal = true;
         Game.state.KEY_STATUS[eKeyVal] = toggleVal;
         if (e.key === ' ') Game.state.KEY_STATUS['space'] = toggleVal;
-        if (e.key === 'p') {
-            if (Game.state.isPaused) {
-                Game.state.isPaused = false;
-                mainLoop();
-            } else {
-                if(Game.innerState !== ("waiting" || "boot" || "reset")) Game.state.isPaused = true;
-            }
-        }
+        if (e.key === 'p') handleGamePause();
     }
 };
 
